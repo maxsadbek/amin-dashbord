4888dbfe1b5eb8ce3a406dc1aa8dc3d91f068c13
maxsadbek <masadanb@gmail.com>
2026-02-05 14:26:38 +0500

    add

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -16,12 +16,59 @@ import {
 
 const router = createBrowserRouter([
   {
-    path: '/'
-  }
-])
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+        errorElement: <Error />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+        errorElement: <Error />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
 
 const App = () => {
-  return <div></div>;
+  <QueryClientProvider>
+    <RouterProvider />
+    <ReactQueryDevtools />
+  </QueryClientProvider>;
 };
 
 export default App;
--- admin/src/pages/Products.jsx
+++ admin/src/pages/Products.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>
+    <div className='flex items-center justify-center'>
 
     </div>
   )
